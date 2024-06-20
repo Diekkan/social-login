1a888e604e02ec34fcca0280bb5f728a635a31f7
Diekkan <91082485+Diekkan@users.noreply.github.com>
2024-06-20 02:06:35 -0300

    Db context initial config

---
+++ Login.Api/Data/AuthContext.cs
@@ -0,0 +1,11 @@
+﻿using Login.Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login.Api.Data
+{
+    public class AuthContext : IdentityDbContext<User>
+    {
+        public AuthContext(DbContextOptions<AuthContext> options) : base(options) { }
+    }
+}
