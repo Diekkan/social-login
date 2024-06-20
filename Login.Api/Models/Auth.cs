f5488c7b9c6a2a98218219f091c008601d0a46c7
Diekkan <91082485+Diekkan@users.noreply.github.com>
2024-06-20 02:04:32 -0300

    User models & auth dtos

---
+++ Login.Api/Models/Auth.cs
@@ -0,0 +1,18 @@
+﻿namespace Login.Api.Models
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword {  get; set; }
+    }
+
+}
---
+++ Login.Api/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Login.Api.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public User(RegisterDTO model)
+        {
+            Email = model.Email;
+            UserName = model.Email;
+            FirstName = model.FirstName;
+            LastName = model.LastName;
+        }
+        public User() { }
+
+    }
+
+    public class UserDTO
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Token {  get; set; }
+        public UserDTO(User user, string token)
+        {
+            Email = user.Email;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Token = token;
+        }
+        public UserDTO(User user)
+        {
+            Email = user.Email;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+        }
+        public UserDTO() { }
+    }
+}
