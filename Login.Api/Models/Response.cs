2779b1d243edf851de1da71b25bc8e3d04e7e77c
Diekkan <91082485+Diekkan@users.noreply.github.com>
2024-06-20 02:04:55 -0300

    Response utility model

---
+++ Login.Api/Models/Response.cs
@@ -0,0 +1,23 @@
+﻿namespace Login.Api.Models
+{
+    public class Response
+    {
+        public bool Succeded { get; set; }
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public object Body { get; set; }
+
+        public Response() { }
+        public Response(string message) {
+            Succeded = false;
+            Message = message;
+        }
+        public Response(object body)
+        {
+            Body = body;
+            Succeded = true;
+        }
+
+
+    }
+}
