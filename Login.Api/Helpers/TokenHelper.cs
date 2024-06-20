4b913d566b301f993c85ca90cbbb652c98fa3206
Diekkan <91082485+Diekkan@users.noreply.github.com>
2024-06-20 02:06:08 -0300

    Token handler

---
+++ Login.Api/Helpers/TokenHelper.cs
@@ -0,0 +1,30 @@
+﻿using Login.Api.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Login.Api.Helpers
+{
+    public static class TokenHelper
+    {
+        public static string GenerateToken(User user, string jwtSecret)
+        {
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim("UserID", user.Id.ToString()));
+            //Add claims as wished here, could be email, or some other info that would be like to be handled by the JWT Token. 
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(2),
+                SigningCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret)), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(securityToken);
+            return token;
+        }
+    }
+}
